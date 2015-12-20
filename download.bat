3ff4d10ef18a3e50ad89ea786f4788406c74b8d5
mib008 <mib008@126.com>
2015-12-20 23:44:33 +0800

    1221

--- download.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\mib008\Documents\YUZUSOFT\サノバウィッチ\savedata
-git pull origin master
--- upload.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\mib008\Documents\YUZUSOFT\サノバウィッチ\savedata
-git add .
-git commit -m "1221"
-git push origin master
